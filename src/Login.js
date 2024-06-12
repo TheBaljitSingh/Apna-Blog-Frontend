d8d844819ea448fac3071d3f1638aa9970a2f23e
thebaljitsingh <thebaljitsinghin@gmail.com>
2024-06-13 00:25:37 +0530

    updated the request url

--- src/Login.js
+++ src/Login.js
@@ -45,22 +45,14 @@ export default function Login(){
   
 
     const navigate = useNavigate();
-    // const dispatch = useDispatch();
-    // const {isAuthenticated} = useSelector(state=>state.custom);
-    // console.log(isAuthenticated);
+
   
     const loginBtn = ()=>{
         console.log("login wala btn call hua");
 
         // decode the cookie
 
-
-    //   store.dispatch({
-    //     type:"checkLogin",
-    //     payload: true,
-    //   })
-
-      
+ 
     };
     
   
@@ -78,7 +70,7 @@ export default function Login(){
         setPassword("");
 
 
-        await axios.post(`${process.env.REACT_APP_BACKEND_URL}auth/login`,{email, password})
+        await axios.post(`${process.env.REACT_APP_BACKEND_URL}/auth/login`,{email, password})
         .then(res =>{   
             
             if(res.status===200){
@@ -152,7 +144,7 @@ export default function Login(){
             // console.log("signup successfully");
 
             
-            await axios.post(`${process.env.REACT_APP_BACKEND_URL}auth/register`, {name,email, password,cpassword})
+            await axios.post(`${process.env.REACT_APP_BACKEND_URL}/auth/register`, {name,email, password,cpassword})
             .then(res=>{
                 console.log(res);
                 if(res.status==201){
