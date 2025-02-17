4af0e8dfb4c3e2d86afadbf032dc2a1703fea6f7
Baljit Singh <thebaljitsinghin@gmail.com>
2025-02-17 12:44:45 +0530

    update: added default user for visitor login

--- src/Login.js
+++ src/Login.js
@@ -163,11 +163,11 @@ export default function Login(){
                         <form class="space-y-4 md:space-y-6" method="post" onSubmit={loginSubmit} >
                             <div>
                                 <label for="email" class="block mb-2 text-sm font-medium text-gray-900 ">Your email</label>
-                                <input required id="email" type="email" value={email} onChange={(e) => setEmail(e.target.value)}   class="bg-gray-50 border border-gray-300 outline-none text-gray-900 sm:text-sm rounded-lg  focus:border-primary-600 block w-full p-2.5 " placeholder="name@gmail.com" />
+                                <input required id="email" type="email" value={email} onChange={(e) => setEmail(e.target.value)}   class="bg-gray-50 border border-gray-300 outline-none text-gray-900 sm:text-sm rounded-lg  focus:border-primary-600 block w-full p-2.5 " placeholder="user@gmail.com" />
                             </div>
                             <div>
                                 <label for="password" class="block mb-2 text-sm font-medium text-gray-900 ">Password</label>
-                                <input required id="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)}  name="password" placeholder="••••••••" class="bg-gray-50 border outline-none border-gray-300 text-gray-900 sm:text-sm rounded-lg  focus:border-primary-600 block w-full p-2.5 " />
+                                <input required id="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)}  name="password" placeholder="password" class="bg-gray-50 border outline-none border-gray-300 text-gray-900 sm:text-sm rounded-lg  focus:border-primary-600 block w-full p-2.5 " />
                             </div>
                             <div class="flex items-center justify-between">
                                 <div class="flex items-start">
